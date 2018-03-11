7ccd033485ab61e816f7db4482c12c335d7706a8
Jonathan Funk <jonfunk88@hotmail.com>
2018-03-10 16:34:58 -0800

    main components made

---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Order extends Component {
+  render() {
+    return (
+      <div className="order">
+        <h2>Order</h2>
+      </div>
+    );
+  }
+}
+
+export default Order;
