c7a4ac677b2731426cfec91effc2b2823a91c924
Jonathan Funk <jonfunk88@hotmail.com>
2018-03-11 16:09:18 -0700

    orders can now be stored to localstorage

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,8 @@ class Order extends Component {
   renderOrder = key => {
     const fish = this.props.fishes[key]
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    if(!fish) return null;
     if(!isAvailable){
       return (
         <li key={key}>
@@ -28,7 +29,7 @@ class Order extends Component {
       const fish = this.props.fishes[key]
       const count = this.props.order[key];
       const isAvailable = fish && fish.status === 'available';
-      if (isAvailable){
+      if(isAvailable){
         return prevTotal + (count * fish.price)
       }
       return prevTotal
